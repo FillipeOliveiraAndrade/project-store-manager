87c7d128cb3a61f67c7433ff30a22e8e94bad956
Fillipe Oliveira <fillipe.trybe@gmail.com>
2022-12-08 00:01:16 -0300

    Adiciona testes da função getProductsById do controller

--- tests/unit/controllers/productsController.test.js
+++ tests/unit/controllers/productsController.test.js
@@ -10,8 +10,8 @@ const { productsService } = require('../../../src/services');
 const { productsController } = require('../../../src/controllers');
 
 describe('Testes de unidade do controller de produtos', () => {
-  describe('Lista todas os produtos', () => {
-    it('listando produtos', async () => {
+  describe('Listando produtos', () => {
+    it('lista todos os produtos', async () => {
       const req = {}
       const res = {}
 
@@ -25,5 +25,37 @@ describe('Testes de unidade do controller de produtos', () => {
       expect(res.status).to.have.been.calledWith(200);
       expect(res.json).to.have.been.calledWith(product);
     });
+
+    it('lista um produto específico recuperado pelo ID', async () => {
+      const req = { params: { id: 1 } };
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsService, 'findById')
+        .resolves({ type: null, message: product[0] });
+
+      await productsController.getProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(product[0]);
+    });
+
+    it('retorna um erro caso não encontre o produto', async () => {
+      const req = { params: { id: 999 } };
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsService, 'findById')
+        .resolves({ type: 'PRODUCT_NOT_FOUND', message: 'Product not found' });
+
+      await productsController.getProductById(req, res);
+
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+    });
+
+    afterEach(sinon.restore);
   });
 });
